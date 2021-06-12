ef1b4d869611c18502ff33661ce5f6b2c0fe3135
hasanuzzman-dev <hasanuzzaman.dev@gmail.com>
2021-06-12 22:53:23 +0600

    getting location by using geoLocator

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_clima/screens/loading_screen.dart';
 
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
